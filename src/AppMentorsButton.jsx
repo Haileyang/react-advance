fe14228086e85066deb7f6f58de58c80d3f33b49
Hailey Yang <yanghyeri73@gmail.com>
2023-03-18 19:15:18 -0700

    Performance (refactor) - useCallBack

--- src/AppMentorsButton.jsx
+++ src/AppMentorsButton.jsx
@@ -1,51 +1,52 @@
-import React, { useReducer, useMemo } from 'react';
+import React, { useReducer, useMemo, useCallback, useCallBack } from 'react';
 import personReducer from './reducer/person-reducer';
 
 export default function AppMentorsButton() {
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
+    const [person, dispatch] = useReducer(personReducer, initialPerson);
 
-  const handleUpdateMentor = () => {
-    const prev = prompt(`whose name you'd like to change?`);
-    const current = prompt(`Which name you'd like to change to?`);
-    dispatch({type: 'updated', prev, current})
-  }
+    //refactoring 02 - useCallBack
+    const handleUpdateMentor = useCallBack (() => {
+        const prev = prompt(`whose name you'd like to change?`);
+        const current = prompt(`Which name you'd like to change to?`);
+        dispatch({type: 'updated', prev, current})
+    }, [])
 
-  const handleAddMentor = () => {
-    const name = prompt(`What is name of new mentor?`);
-    const title = prompt(`What is title of new mentor?`);
-    dispatch({type: 'added', name, title})
-  }
+    const handleAddMentor = useCallBack (() => {
+        const name = prompt(`What is name of new mentor?`);
+        const title = prompt(`What is title of new mentor?`);
+        dispatch({type: 'added', name, title})
+    }, [])
 
-  const handleDeleteMentor = () => {
-    const name = prompt(`whose you'd like delete?`);
-    dispatch({type: 'deleted', name})
-  }
+    const handleDeleteMentor = useCallBack (() => {
+        const name = prompt(`whose you'd like delete?`);
+        dispatch({type: 'deleted', name})
+    }, [])
 
-  return (
-    <div>
-      <h1>
-        {person.name}is {person.title}
-      </h1>
-      <p>{person.name}'s mentor:</p>
-      <ul>
-        {person.mentors.map((mentor, index) => (
-          <li key={index}>
-            {mentor.name} ({mentor.title})
-          </li>
-        ))}
-      </ul>
-      <Button text="Change Mentor's Name" onClick={handleUpdateMentor}></Button>
-      <Button text="Add Mentor" onClick={handleAddMentor}></Button>
-      <Button text="Delete Mentor" onClick={handleDeleteMentor}></Button>
-    </div>
-  );
+    return (
+        <div>
+        <h1>
+            {person.name}is {person.title}
+        </h1>
+        <p>{person.name}'s mentor:</p>
+        <ul>
+            {person.mentors.map((mentor, index) => (
+            <li key={index}>
+                {mentor.name} ({mentor.title})
+            </li>
+            ))}
+        </ul>
+        <Button text="Change Mentor's Name" onClick={handleUpdateMentor}></Button>
+        <Button text="Add Mentor" onClick={handleAddMentor}></Button>
+        <Button text="Delete Mentor" onClick={handleDeleteMentor}></Button>
+        </div>
+    );
 }
 
 function Button({ text, onClick }) {
   console.log('Button', text, 're-rendering 😜');
     // const result = calculateSomething() //Time consuming 
     //refactoring 01 - useMemo
-    const result =  useMemo(() => calculateSomething(), [])
+    // const result =  useMemo(() => calculateSomething(), [])
 
   return (
     <button
