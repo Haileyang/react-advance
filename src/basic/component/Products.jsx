e6e3df44525daadccd8e976b3abbdefef3864fa8
Hailey Yang <yanghyeri73@gmail.com>
2023-03-18 19:37:21 -0700

    Add Loading and Error

--- src/basic/component/Products.jsx
+++ src/basic/component/Products.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked(prev => !prev)
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined)
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥Data Recieves from Network');
         setProducts(data);
-      });
+      })
+      .catch((e) => setError('Error!!'))
+      .finally(() => setLoading(false))
     return () => { //when component unmount, execute
       console.log('🧹 Cleaning Data');
     };
   }, [checked]);
 
+  if(loading) return <p>Loading ...</p>
+  if(error) return <p>{error}</p>
+  
   return (
     <>
       <input id="checkbox" type="checkbox" value={checked} onChange={handleChange}/>
